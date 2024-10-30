45265e85d9b40a4e3ed8dd852a1e263716f43ebc
Francis Alcantara <101628141+freyncis@users.noreply.github.com>
2024-10-30 20:08:42 +0800

    FIGURED OUT HOW TO GET IT TO SAY ROCK PAPER OR SCISSORS RANDOMLY

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 1){
+        return "Rock";
+    };
+    if (computerChoice === 2){
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
